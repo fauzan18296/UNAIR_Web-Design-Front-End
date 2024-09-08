73f64e29aedca39bd86c3644b796ca7d6cd02f1f
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-08 15:22:06 +0700

    add: +gambar API di folder component menu

--- src/Components/MenuComp.jsx
+++ src/Components/MenuComp.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import React from 'react'
 
@@ -10,9 +10,16 @@ const MenuComp = React.forwardRef((props,ref) => {
   const [image4, setImage4] = useState('')
   const [image5, setImage5] = useState('')
   const [image6, setImage6] = useState('')
+  const [image7, setImage7] = useState('')
+  const [image8, setImage8] = useState('')
+  const [image9, setImage9] = useState('')
+  const [image10, setImage10] = useState('')
+  const [image11, setImage11] = useState('')
+  const [image12, setImage12] = useState('')
   const [error, setError] = useState(null)
 
-    // API Gambar Menu
+  // API Gambar Menu
+  // 1. API gambar dengan pengambilan id 1
     axios.get('https://affectionate-perfection-production.up.railway.app/image/1', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -22,7 +29,7 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
-    
+      // 2. API gambar dengan pengambilan id 2
     axios.get('https://affectionate-perfection-production.up.railway.app/image/2', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -32,7 +39,7 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
-    
+      // 3. API gambar dengan pengambilan id 3
     axios.get('https://affectionate-perfection-production.up.railway.app/image/3', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -42,7 +49,7 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
-    
+      // 4. API gambar dengan pengambilan id 4
     axios.get('https://affectionate-perfection-production.up.railway.app/image/4', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -52,7 +59,7 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
-    
+      // 5. API gambar dengan pengambilan id 5
     axios.get('https://affectionate-perfection-production.up.railway.app/image/5', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -62,7 +69,7 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
-    
+      // 6. API gambar dengan pengambilan id 6
     axios.get('https://affectionate-perfection-production.up.railway.app/image/6', {responseType: 'blob'})
       .then(response => {
         const imageUrl = URL.createObjectURL(response.data);
@@ -72,13 +79,73 @@ const MenuComp = React.forwardRef((props,ref) => {
         console.error('Error fetching the image:', error)
         setError('Failed to load image')
       })
+      // 7. API gambar dengan pengambilan id 7
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/7', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage7(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
+      // 8. API gambar dengan pengambilan id 8
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/8', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage8(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
+      // 9. API gambar dengan pengambilan id 9
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/9', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage9(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
+      // 10. API gambar dengan pengambilan id 10
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/10', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage10(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
+      // 11. API gambar dengan pengambilan id 11
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/11', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage11(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
+      // 12. API gambar dengan pengambilan id 12
+    axios.get('https://affectionate-perfection-production.up.railway.app/image/12', {responseType: 'blob'})
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+          setImage12(imageUrl)
+      })
+      .catch(error => {
+        console.error('Error fetching the image:', error)
+        setError('Failed to load image')
+      })
 
 
   return (
     <div ref={ref} className='menu-kuliner' >
+       <h2 className='text-center mt-3'>Menu Food & Drink</h2>
       <Container className='d-flex justify-content-center align-items-center'>
-        <Row className='d-flex justify-content-center'>
-          <h2 className='mt-3'>Menu Food & Drink</h2>
+        <Row className='d-flex align-items-center  row-cols-lg-5 row-cols-md-3 row-cols-sm-2 mt-2 g-4'>
           {/* Memunculkan API gambar ke website */}
           <Col className='my-3'>
             {image1 ? (
@@ -145,6 +212,78 @@ const MenuComp = React.forwardRef((props,ref) => {
               <>
                 <img src={image6} alt='image6' />
                 <p className='menu-name  fw-semibold'>Sourdough Bread</p>
+                <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image7 ? (
+              <>
+                <img src={image7} alt='image7' />
+                <p className='menu-name  fw-semibold'>Kiwi Mojito</p>
+                <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image8 ? (
+              <>
+                <img src={image8} alt='image8' />
+                <p className='menu-name  fw-semibold'>Manggo Chia Smootie</p>
+                <button className='mt-3 btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image9 ? (
+              <>
+                <img src={image9} alt='image9' />
+                <p className='menu-name  fw-semibold'>Orange juice</p>
+                <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image10 ? (
+              <>
+                <img src={image10} alt='image10' />
+                <p className='menu-name  fw-semibold'>Red Cocktail</p>
+                <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image11 ? (
+              <>
+                <img src={image11} alt='image11' />
+                <p className='menu-name  fw-semibold'>Lemon Tea</p>
+                <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
+              </>
+            ) : (
+                error ? <p>{ error }</p>: <p>Loading...</p>
+            )}
+          </Col>
+
+          <Col className='my-3'>
+            {image12 ? (
+              <>
+                <img src={image12} alt='image12' />
+                <p className='menu-name  fw-semibold'>Spear Mint</p>
                 <button className='btn-menu fw-semibold p-1'>Pesan Sekarang!</button>
               </>
             ) : (
