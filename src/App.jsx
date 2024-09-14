d04f75f0aeb55387427664c2e8c8922e08767551
Ahmad Fauzan <123647118+fauzan18296@users.noreply.github.com>
2024-09-14 09:57:36 +0700

    add: + pageCategory route (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home/HomePage'
 import About from './Pages/About/AboutPage'
-
+import Categories from './Pages/Categories/CategoriesPage'
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/category' element={<Categories />} />
       </Routes>
       </Router>
       </>
