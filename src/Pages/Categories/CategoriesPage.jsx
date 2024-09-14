a25a54f861e4b08ddd14ad29b99e8c1f8d137996
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-14 12:24:27 +0700

    add: initial add

--- src/Pages/Categories/CategoriesPage.jsx
+++ src/Pages/Categories/CategoriesPage.jsx
@@ -4,9 +4,9 @@ import CategoriesComp from "../../Components/CategoriesComp";
 const Categories = () => {
   return (
     <>
-     <NavbarComp/>
-    <CategoriesComp />
+    <NavbarComp />
+   <CategoriesComp/>   
     </>
   )
 }
-export default Categories
+export default  Categories
