258dcfd5ed1894a47b2b7c8d9c6bb41ea3d5f95c
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-14 09:51:18 +0700

    add: + pageCategory route

---
+++ src/Components/CategoriesComp.jsx
@@ -0,0 +1,12 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const CategoriesComp = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+export default CategoriesComp
---
+++ src/Pages/Categories/CategoriesPage.jsx
@@ -0,0 +1,13 @@
+import NavbarComp from "../../Components/NavbarComp";
+import CategoriesComp from "../../Components/CategoriesComp";
+
+const Categories = () => {
+  return (
+    <>
+    <NavbarComp />
+   <CategoriesComp/>   
+    </>
+  )
+}
+
+export default  Categories
