d6021bb109ecbbf936dd0e9fdc5984b3841d38b3
Ahmad Fauzan <123647118+fauzan18296@users.noreply.github.com>
2024-09-17 21:59:02 +0700

    Staging (#16)
    * add: initial add
    
    * add: process pertama page about masih ada errornya
    
    * add:+gambar dan content dari halaman about
    
    * style: merubah style navigasi dan halaman about
    
    ---------
    
    Co-authored-by: Abiyansa <abiyan.samawi.alkindi@gmail.com>

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -1,23 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
-<<<<<<< HEAD
-import about from "../assets/IMG/img-about/image1.webp";
-=======
 import about1 from "../assets/IMG/img-about/image1.webp";
->>>>>>> staging
 
 const AboutComp = () => {
   return (
     <div className="about-page d-flex justify-content-center align-items-center">
-<<<<<<< HEAD
-      <img src={about} alt="" />
-      <h2>
-        Kenyamanan anda <span>kepuasan</span> kami
-      </h2>
-      <Container className="about-container d-flex align-items-center">
-        <Row className="d-flex align-items-center">
-          <Col className="fs-3">
-            <p>
-=======
       <Container className="d-flex justify-content-center align-items-center">
         <Row className="d-flex justify-content-center align-items-center">
              <img src={about1} alt="" />
@@ -26,7 +12,6 @@ const AboutComp = () => {
         Kenyamanan anda <span>kepuasan</span> kami
       </h2>
             <p className="fs-5">
->>>>>>> staging
               Kami menghadirkan makanan dan minuman berkualitas tinggi yang
               dapat menggugah selara anda. Dengan pengalaman service yang
               menyenankan dan suasana dalam restaurant yang memanjakan.
