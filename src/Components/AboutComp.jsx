db591f3076a8e4ce6fbec7d6736557f7f09323b3
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-17 21:38:04 +0700

    style: merubah style navigasi dan halaman about

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -1,17 +1,17 @@
 import { Container, Row, Col } from "react-bootstrap";
-import about from "../assets/IMG/img-about/image1.webp";
+import about1 from "../assets/IMG/img-about/image1.webp";
 
 const AboutComp = () => {
   return (
     <div className="about-page d-flex justify-content-center align-items-center">
-      <img src={about} alt="" />
-      <h2>
+      <Container className="d-flex justify-content-center align-items-center">
+        <Row className="d-flex justify-content-center align-items-center">
+             <img src={about1} alt="" />
+          <Col className="mx-auto mt-3">
+      <h2 className="text-center">
         Kenyamanan anda <span>kepuasan</span> kami
       </h2>
-      <Container className="about-container d-flex align-items-center">
-        <Row className="d-flex align-items-center">
-          <Col className="fs-3">
-            <p>
+            <p className="fs-5">
               Kami menghadirkan makanan dan minuman berkualitas tinggi yang
               dapat menggugah selara anda. Dengan pengalaman service yang
               menyenankan dan suasana dalam restaurant yang memanjakan.
