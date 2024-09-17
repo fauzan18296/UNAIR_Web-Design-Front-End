0856f76e8d94161782f8f946fb1a7146ba247215
Ahmad Fauzan <fauzan18296@gmail.com>
2024-09-17 21:42:41 +0700

    add: merge code staging to main branch or to production

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -1,9 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
+<<<<<<< HEAD
 import about from "../assets/IMG/img-about/image1.webp";
+=======
+import about1 from "../assets/IMG/img-about/image1.webp";
+>>>>>>> staging
 
 const AboutComp = () => {
   return (
     <div className="about-page d-flex justify-content-center align-items-center">
+<<<<<<< HEAD
       <img src={about} alt="" />
       <h2>
         Kenyamanan anda <span>kepuasan</span> kami
@@ -12,6 +17,16 @@ const AboutComp = () => {
         <Row className="d-flex align-items-center">
           <Col className="fs-3">
             <p>
+=======
+      <Container className="d-flex justify-content-center align-items-center">
+        <Row className="d-flex justify-content-center align-items-center">
+             <img src={about1} alt="" />
+          <Col className="mx-auto mt-3">
+      <h2 className="text-center">
+        Kenyamanan anda <span>kepuasan</span> kami
+      </h2>
+            <p className="fs-5">
+>>>>>>> staging
               Kami menghadirkan makanan dan minuman berkualitas tinggi yang
               dapat menggugah selara anda. Dengan pengalaman service yang
               menyenankan dan suasana dalam restaurant yang memanjakan.
--- src/Components/NavbarComp.jsx
+++ src/Components/NavbarComp.jsx
@@ -2,19 +2,17 @@ import {Navbar, Container, Nav} from 'react-bootstrap'
 
 const NavbarComp = () => {
   return (
-     <Navbar fixed='top' className='glass-navbar navbar-nav d-flex align-items-center'>
+    <Navbar fixed='top' className='glass-navbar navbar-nav d-flex align-items-center'>
       <Container className='d-flex justify-content-center'>
-         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className='me-auto'>
-          <Nav.Link href="/about" className='text-black fs-5 fw-semibold'>About</Nav.Link>
-        </Nav>
-         <Navbar.Brand href='/' className='mx-auto fs-4 fw-bold navbar-brand'>
+          <Navbar.Brand href='/' className='fs-4 fw-bold'>
             <span className='fw-bold'>ASAFY</span>
           </Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='ms-auto'>
-           <Nav.Link href="/category" className='text-black fs-5 fw-semibold'>Categories</Nav.Link>
-          </Nav>
+            <Nav.Link href="/about" className='text-black fs-5 fw-semibold'>About</Nav.Link>
+             <Nav.Link href="/category" className='text-black fs-5 fw-semibold'>Categories</Nav.Link>
+        </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
