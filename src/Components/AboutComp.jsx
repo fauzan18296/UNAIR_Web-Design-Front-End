fa16cc8f5ee4a97809705608a3d0a43486fecebb
Abiyansa <abiyan.samawi.alkindi@gmail.com>
2024-09-17 14:22:16 +0700

    fix: fi conflict code in folder css and about component

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -1,18 +1,28 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap";
+import about from "../assets/IMG/img-about/image1.webp";
 
 const AboutComp = () => {
   return (
-    <div className="about-page d-flex align-items-center">
-    <h2 className="text-center">About</h2>
-      <Container className="about-container d-flex justify-content-center align-items-center">
-      <Row className="d-flex justify-content-center align-items-center">
+    <div className="about-page d-flex justify-content-center align-items-center">
+      <img src={about} alt="" />
+      <h2>
+        Kenyamanan anda <span>kepuasan</span> kami
+      </h2>
+      <Container className="about-container d-flex align-items-center">
+        <Row className="d-flex align-items-center">
           <Col className="fs-3">
-        <p>Kami menghadirkan makanan dan minuman berkualitas tinggi yang dapat menggugah selara anda. Dengan pengalaman service yang menyenankan dan suasana dalam restaurant yang memanjakan. Bahan-bahan makanan dan minuman  yang digunakan halal dan terjamin cita rasanya.</p>
-        </Col>
-      </Row>
+            <p>
+              Kami menghadirkan makanan dan minuman berkualitas tinggi yang
+              dapat menggugah selara anda. Dengan pengalaman service yang
+              menyenankan dan suasana dalam restaurant yang memanjakan.
+              Bahan-bahan makanan dan minuman yang digunakan halal dan terjamin
+              cita rasanya.
+            </p>
+          </Col>
+        </Row>
       </Container>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default AboutComp;
