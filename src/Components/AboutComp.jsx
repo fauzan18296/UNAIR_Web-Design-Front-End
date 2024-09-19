00cb54f86078ecfc6cc445a5812c90049e8250e9
Abiyansa <abiyan.samawi.alkindi@gmail.com>
2024-09-19 11:44:57 +0700

    add: +animasi di page about

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -1,13 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
 import about1 from "../assets/IMG/img-about/image1.webp";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const AboutComp = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+      delay: 20,
+      easing: "ease-in-out-back",
+    });
+  });
   return (
     <div className="about-page d-flex justify-content-center align-items-center">
       <Container className="d-flex justify-content-center align-items-center">
         <Row className="d-flex justify-content-center align-items-center">
-          <img src={about1} alt="" />
-          <Col className="mx-auto mt-3">
+          <img data-aos="fade-right" src={about1} alt="" />
+          <Col data-aos="fade-left" className="mx-auto mt-3">
             <h2 className="text-center">
               Kenyamanan anda <span>kepuasan</span> kami
             </h2>
